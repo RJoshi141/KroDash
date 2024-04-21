6d63ec487c3c8c3a4cd8c381d52774ff085c6a5a
roshnik1 <roshnikhatri87@gmail.com>
2024-04-21 02:55:17 -0400

    new changes

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js on Windows!');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    // You can handle registration logic here, like saving to a database
+    
+    // Respond with a simple message for now
+    res.send('Registration successful!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
